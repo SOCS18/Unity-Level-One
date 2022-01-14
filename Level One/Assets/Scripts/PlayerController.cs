025d4b1f794b95fa95cf8d76f7264440ab75d6ab
Saul Carreon <sacarreon18@gmail.com>
2022-01-13 23:16:15 -0600

    Fixed Movement
    Created FixedUpdate so that object doesn't jitter on wall, and I made the player turn left and right by rotating object, and made sure player only jumped once

--- Level One/Assets/Scripts/PlayerController.cs
+++ Level One/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,61 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] private float playerMovement;
     [SerializeField] private float playerSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
-    
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isTouchingWall;
+
     // Start is called before the first frame update
     void Start()
     {
         if (!rb)
             rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
+    /*
     void Update()
     {
-        float playerMovement = Input.GetAxis("Horizontal");
-
-        transform.Translate(playerMovement * playerSpeed * Time.deltaTime, 0, 0);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    */
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerMovement = Input.GetAxis("Horizontal");
+
+        if (playerMovement > 0 && playerMovement < 1)
+            playerMovement = 1;
+        if (playerMovement < 0 && playerMovement > -1)
+            playerMovement = -1;
+
+        if (playerMovement < 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
+            transform.Translate(-playerMovement * Time.deltaTime * playerSpeed, 0, 0);
+        }
+        if (playerMovement > 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+            transform.Translate(playerMovement * Time.deltaTime * playerSpeed, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+        if (collision.gameObject.tag == "Wall")
+            isTouchingWall = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = false;
+        if (collision.gameObject.tag == "Wall")
+            isTouchingWall = false;
     }
 }
